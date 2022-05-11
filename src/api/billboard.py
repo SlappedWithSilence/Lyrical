041438202fdcd702ec6f329ee35cc2eba8368a19
Slapped with Silence <asigal55+02@gmail.com>
2022-05-10 21:12:25 -0500

    Added Top 100 runner

--- src/api/billboard.py
+++ src/api/billboard.py
@@ -2,4 +2,13 @@
 
     Ex: Billboard Top 100, 2009
 """
+from billboard import ChartData
 
+
+class BillboardRunner:
+
+    def __init__(self):
+        pass
+
+    def get(self, year: int, chart: str = "hot-100-songs") -> ChartData:
+        return ChartData(chart, year=year)
