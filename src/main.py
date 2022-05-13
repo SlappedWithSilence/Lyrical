da5a739ad109f7538451724eef98455fc68c90e9
Slapped With Silence <asigal55+02@gmail.com>
2022-05-12 19:17:35 -0500

    Added lifecycle functions

--- src/main.py
+++ src/main.py
@@ -1,12 +1,40 @@
-from api.lyrics import AZLyricRunner, SongContainer
+from multiprocessing.dummy import Pool as ThreadPool
+
+from api.lyrics import get_lyrics, SongContainer
+from api.billboard import BillboardRunner
+from persistance.cache import get_cache
 
 
-# Lifecycle Functions
-from src.api.billboard import BillboardRunner
-
-
-def __gather() -> None:
+def __setup():
+    """Perform system setup functions"""
     pass
+
+
+def __interpret():
+    """Generate a list of content to gather from user input"""
+    pass
+
+
+def __gather(years: list[int], songs: list[SongContainer]) -> None:
+    """Gather requested content from third-party sources"""
+    # Set up runners
+    runner = BillboardRunner()
+
+    # Get charts
+    for year in years:
+        if year not in get_cache()["charts"]["yearly"]:
+            songs = runner.get(year=year)
+            get_cache()["charts"]["yearly"][year] = songs
+
+    # Get lyrics
+    worker_pool = ThreadPool(1)
+
+    for song in songs:
+        get_lyrics(song)
+
+    lyrics = worker_pool.map(get_lyrics, songs.entries)
+
+    print(lyrics[15])
 
 
 def __clean() -> None:
@@ -26,12 +54,10 @@ def __export() -> None:
 
 
 if __name__ == "__main__":
-    test_song = SongContainer("Three Days Grace", "Riot")
-    runner = AZLyricRunner()
-
-    result = runner.get(test_song)
-
-    print(result.lyrics)
-
-    runner = BillboardRunner()
-    print(runner.get(year=2006).entries)
+    __setup()
+    __interpret()
+    __gather(years=[2007], songs=[SongContainer(artist="Rise Against", title="Satellite")])
+    __clean()
+    __analyze()
+    __compare()
+    __export()
